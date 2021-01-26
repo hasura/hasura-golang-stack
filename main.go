ee7e9d1e10eacdfb528b6f931bd88516b2a7aff7
Adron Hall <adron@hasura.io>
2021-01-26 10:45:41 -0800

    Adding first commit of skeleton CLI.

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package main
 
-import "github.com/adron/hasura-golang-stack/cmd"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
 
 func main() {
-  cmd.Execute()
+	jsonData := map[string]string{
+		"query": `{
+		  tododeluge_Todo {
+			Id			
+			Details
+			Todo
+		  }
+		}`,
+	}
+	jsonValue, _ := json.Marshal(jsonData)
+	request, err := http.NewRequest("POST", "https://graphql-go-workflow.hasura.app/v1/graphql", bytes.NewBuffer(jsonValue))
+	client := &http.Client{Timeout: time.Second * 10}
+	response, err := client.Do(request)
+	defer response.Body.Close()
+	if err != nil {
+		fmt.Printf("Failure Failure!: %s \n", err)
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(data))
 }
