25fe8d12d664eba3705c8d1c0f361fc706c84deb
Adron Hall <adron@hasura.io>
2021-02-02 10:21:40 -0800

    Added mod support, turned on mod support.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hasura/hasura-golang-stack
+
+go 1.15
+
+require github.com/spf13/cobra v1.1.1
--- main.go
+++ main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
-)
+import "github.com/spf13/cobra/cobra/cmd"
 
 func main() {
-	jsonData := map[string]string{
-		"query": `{
-		  tododeluge_Todo {
-			Id			
-			Details
-			Todo
-		  }
-		}`,
-	}
-	jsonValue, _ := json.Marshal(jsonData)
-	request, err := http.NewRequest("POST", "https://graphql-go-workflow.hasura.app/v1/graphql", bytes.NewBuffer(jsonValue))
-	client := &http.Client{Timeout: time.Second * 10}
-	response, err := client.Do(request)
-	defer response.Body.Close()
-	if err != nil {
-		fmt.Printf("Failure Failure!: %s \n", err)
-	}
-	data, _ := ioutil.ReadAll(response.Body)
-	fmt.Println(string(data))
+	cmd.Execute()
 }
